c9dde360119539940033dd68009324f5f47f7810
Wagner Mattos <wagner_mattos@msn.com>
2019-06-17 10:52:11 -0300

    Set morgan

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const routes = require('./routes');
 
@@ -6,6 +7,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan('dev'));
 
 app.use(routes);
 
