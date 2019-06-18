99e9b8c73df17fe815c96721b160a9c32cd5794e
Wagner Mattos <github@wagnermattos.com>
2019-06-18 16:54:30 -0300

    feat: set CORS

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
 });
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
